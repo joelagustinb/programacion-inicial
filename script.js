a89badfd4130d6c5293bf513146fd40bdf20c94a
joelagustinb <joelagustinbarbona@gmail.com>
2023-04-20 12:56:47 -0300

    JAVASCRIPT Inicio

---
+++ script.js
@@ -0,0 +1,13 @@
+let nombre = "Joel";
+let apellido = "Barbona";
+const edad = 19;
+let mascota = "Sin mascota"
+
+console.log(nombre);
+console.log(apellido);
+console.log(edad);
+console.log(mascota);
+
+let a= nombre + apellido;
+
+console.log(a);
